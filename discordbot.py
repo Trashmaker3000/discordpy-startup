85a736949a24c10427e8c04a9a82905fc5c40c30
Trashmaker3000 <79903831+Trashmaker3000@users.noreply.github.com>
2021-03-03 00:26:02 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -6,6 +6,16 @@ bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 @client.event
-async def ctx.send('起動しました')
+async def on_ready():
+    CHANNEL_ID = "816248943549349918"
+    channel = client.get_channel(CHANNEL_ID)
+    await message.channel.send('```起動しました```')
+ 
+@bot.event
+async def on_command_error(ctx, error):
+    orig_error = getattr(error, "original", error)
+    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
+    await ctx.send(error_msg)
+
 
 bot.run(token)
