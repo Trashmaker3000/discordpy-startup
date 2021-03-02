27b9bbb5a40bf95e90eb1a7e01530be231db9fa9
Trashmaker3000 <79903831+Trashmaker3000@users.noreply.github.com>
2021-03-02 22:59:31 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,11 +1,17 @@
+  
+from discord.ext import commands
 import os
 import traceback
 
-import discord
-from discord.ext import commands
-
-token = "bot_token"
 bot = commands.Bot(command_prefix='/')
+token = os.environ['DISCORD_BOT_TOKEN']
+
+
+@bot.event
+async def on_command_error(ctx, error):
+    orig_error = getattr(error, "original", error)
+    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
+    await ctx.send(error_msg)
 
 @bot.event
 async def on_voice_state_update(member, before, after):
