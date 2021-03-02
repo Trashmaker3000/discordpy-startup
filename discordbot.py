cbff9702ce860cb9ce83489d86430dab9e1190eb
Trashmaker3000 <79903831+Trashmaker3000@users.noreply.github.com>
2021-03-02 23:01:25 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -6,13 +6,6 @@ import traceback
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
-
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
-
 @bot.event
 async def on_voice_state_update(member, before, after):
     if not before.channel and after.channel:
