3487053479f55be9a15fe37e20fc76dad3957b5a
Trashmaker3000 <79903831+Trashmaker3000@users.noreply.github.com>
2021-03-02 23:27:21 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,20 +1,13 @@
+import discord
+
+
+with open("token.txt") as F:
+  token = F.read()
+ 
+client = discord.client()
+
+@client.event
+async def on_ready():
+  print("起動しました。")
   
-from discord.ext import commands
-import os
-import traceback
-
-bot = commands.Bot(command_prefix='/')
-token = os.environ['DISCORD_BOT_TOKEN']
-
-@bot.event
-async def on_voice_state_update(member, before, after):
-    if not before.channel and after.channel:
-        set_mention_name = after.channel.name
-        role = discord.utils.get(member.guild.roles, name=set_mention_name)
-        await member.add_roles(role)
-    elif before.channel and not after.channel:
-        remove_mention_name = before.channel.name
-        role = discord.utils.get(member.guild.roles, name=remove_mention_name)
-        await member.remove_roles(role)
-
-bot.run(token)
+ client.run(token)
