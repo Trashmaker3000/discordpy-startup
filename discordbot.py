030c2c56479d29f5110f435e66474d1992973aa7
Trashmaker3000 <79903831+Trashmaker3000@users.noreply.github.com>
2021-03-02 23:39:51 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -5,8 +5,16 @@ import traceback
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
-@client.event
-async def on_ready():
-  message.channel.send("起動しました")
+
+@bot.event
+async def on_command_error(ctx, error):
+    orig_error = getattr(error, "original", error)
+    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
+    await ctx.send(error_msg)
+
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
   
  client.run(token)
