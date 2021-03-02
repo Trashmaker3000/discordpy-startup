897f2fc8a09fc8166657b3a2fab670670899e925
Trashmaker3000 <79903831+Trashmaker3000@users.noreply.github.com>
2021-03-02 21:14:11 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -5,19 +5,6 @@ import traceback
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
-
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
-
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
-
-
 @bot.event
 async def on_voice_state_update(member, before, after):
     if not before.channel and after.channel:
