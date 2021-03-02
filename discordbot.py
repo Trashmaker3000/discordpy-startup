0011d2610e6183a2ab4459e29647626f2f4883e4
Trashmaker3000 <79903831+Trashmaker3000@users.noreply.github.com>
2021-03-02 19:17:14 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -18,4 +18,20 @@ async def ping(ctx):
     await ctx.send('pong')
 
 
+    token = "bot_token"
+bot = commands.Bot(command_prefix='/')
+
+
+@bot.event
+async def on_voice_state_update(member, before, after):
+    if not before.channel and after.channel:
+        set_mention_name = after.channel.name
+        role = discord.utils.get(member.guild.roles, name=set_mention_name)
+        await member.add_roles(role)
+    elif before.channel and not after.channel:
+        remove_mention_name = before.channel.name
+        role = discord.utils.get(member.guild.roles, name=remove_mention_name)
+        await member.remove_roles(role)
+
+
 bot.run(token)
