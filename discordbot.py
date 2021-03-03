73a50e396ac3e1b14d366131b8c4113d64480a7a
Trashmaker3000 <79903831+Trashmaker3000@users.noreply.github.com>
2021-03-04 02:17:50 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -8,7 +8,6 @@ token = os.environ['DISCORD_BOT_TOKEN']
 @bot.event
 async def　on_voice_state_update(member,before,):
 if len（before.Channele.members） ＝＝ ０：
- await ctx.send('成功')
  
 @bot.command()
 async def ping(ctx):
