e6075993f706b78e360ecff0a95223572111989e
Trashmaker3000 <79903831+Trashmaker3000@users.noreply.github.com>
2021-03-04 00:49:53 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -5,4 +5,13 @@ import traceback
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
+@bot.event
+async def　discord.on_voice_state_update(member,before,after):
+ if len(after.channel.members) == 1: 
+  await create_voice_channel("vc", overwrites=None, category= category, reason=None,)
+ elif len(before.channel.members) == 0:
+  await delete(,channel, reason=None):
+  
+   channel = after.channel
+    
 bot.run(token)
