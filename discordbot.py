93d2118b751f8697f3ea2a8c3ca80f09e2f5ba2c
Trashmaker3000 <79903831+Trashmaker3000@users.noreply.github.com>
2021-03-04 02:27:12 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,1 +1,21 @@
+from discord.ext import commands
+import os
+import traceback
 
+bot = commas.Bot(command_prefix='/')
+token = os.environ['DISCORD_BOT_TOKEN']
+
+
+@bot.event
+async def on_command_error(ctx, error):
+    orig_error = getattr(error, "original", error)
+    errorsg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
+    await ctx.send(error_msg)
+
+
+@bot.coand()
+async def ping(ctx):
+    await ctx.send('pong')
+
+
+bot.run(token)
