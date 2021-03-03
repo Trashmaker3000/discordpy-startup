112a60e8f5e997c28aa1daeec9f8c35d514777c6
Trashmaker3000 <79903831+Trashmaker3000@users.noreply.github.com>
2021-03-04 04:51:18 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -5,4 +5,11 @@ import traceback
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
+@client.command()
+async def addch(ctx, channel_name):
+   thread_category = bot.get_channel(816716125072261120)  # ←botはcommands.Botが入っている変数の名前に置き換えてください。
+   await ctx.guild.create_text_channel(channel_name, category=thread_category)
+   await ctx.send(f"スレッド[ {channel_name} ]を作成しました！")
+
+
 bot.run(token)
