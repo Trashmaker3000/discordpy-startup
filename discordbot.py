9e2a2496b7e37549c6483684ae417e4cd6054fd5
Trashmaker3000 <79903831+Trashmaker3000@users.noreply.github.com>
2021-03-04 02:20:47 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -5,10 +5,6 @@ import traceback
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
-@bot.event
-async def　on_voice_state_update(member,before,):
-if len（before.Channele.members） ＝＝ ０：
- 
 @bot.command()
 async def ping(ctx):
     await ctx.send('ロング')
