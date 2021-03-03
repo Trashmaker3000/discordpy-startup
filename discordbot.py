e0ff308eb891ddfb08ae1f3f0550d12335a2514d
Trashmaker3000 <79903831+Trashmaker3000@users.noreply.github.com>
2021-03-04 03:02:11 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -5,17 +5,4 @@ import traceback
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
-CHANNEL_ID = 816632415165349921
-
-@client.event
-async def on_message(message):
-    if message.author.bot: # メッセージ送信者がBotだった場合は無視する
-        return
-
-    if message.channel.id != CHANNEL_ID: # チャンネルが違う場合は無視する
-        return
-
-    if message.content == '!bot':#「XX」と発言したら
-        await message.channel.send('```BOT discordバージョン起動中です。```')#「XX」が返る処理
-
 bot.run(token)
