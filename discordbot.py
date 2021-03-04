58a86b180dc60fa8531f5ea9880d350d6f22c2e3
Trashmaker3000 <79903831+Trashmaker3000@users.noreply.github.com>
2021-03-04 18:00:44 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -5,14 +5,21 @@ import traceback
 bot = commands.Bot(command_prefix='！')
 token = os.environ['DISCORD_BOT_TOKEN']
 
+
 @client.event
 async def on_ready():
 
-　ch_name = "一般"
- 
-  for channel in client.get_all_channels():
-        if channel.name == ch_name:
-            await channel.send("起動しました")
+　CHANNEL_ID = 816632415165349921 # 任意のチャンネルID(int)
+
+# 任意のチャンネルで挨拶する非同期関数を定義
+async def greet():
+    channel = client.get_channel(CHANNEL_ID)
+    await channel.send('おはよう！')
+
+# bot起動時に実行されるイベントハンドラを定義
+@client.event
+async def on_ready():
+    await greet() # 挨拶する非同期関数
    
 
 bot.run(token)
