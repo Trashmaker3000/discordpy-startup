a632805348acefce6454983f53025c91b99dd155
Trashmaker3000 <79903831+Trashmaker3000@users.noreply.github.com>
2021-03-11 23:43:06 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,16 +1,1 @@
-from discord.ext import commands
-import os
-import traceback
-import datetime
 
-token = os.environ['DISCORD_BOT_TOKEN']
-channel_id = 817830113743863828
-
-@client.event
-async def on_ready():
-    channel = client.get_channel(channel_id)
-    dt_now = str(datetime.datetime.now())
-    embed = discord.Embed(title="on_ready",description=dt_now)
-    await channel.send(embed=embed)
-
-bot.run(token)
