71ae63a11f5a407fb9db0ee1afd4836ca09ff4f9
Trashmaker3000 <79903831+Trashmaker3000@users.noreply.github.com>
2021-03-11 23:38:41 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -3,7 +3,6 @@ import os
 import traceback
 import datetime
 
-bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 channel_id = 817830113743863828
 
@@ -13,16 +12,5 @@ async def on_ready():
     dt_now = str(datetime.datetime.now())
     embed = discord.Embed(title="on_ready",description=dt_now)
     await channel.send(embed=embed)
- 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
 
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send('おはよう')
-    
 bot.run(token)
