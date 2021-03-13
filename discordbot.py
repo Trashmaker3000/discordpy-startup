18268da4ba2d1892834e59ec25784188be4e1701
Trashmaker3000 <79903831+Trashmaker3000@users.noreply.github.com>
2021-03-13 20:16:18 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,1 +1,13 @@
+from discord.ext import commands
+import os
+import traceback
 
+bot = commands.Bot(command_prefix='/')
+token = os.environ['DISCORD_BOT_TOKEN']
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
+
+
+bot.run(token)
